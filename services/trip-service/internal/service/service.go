fb1372dcc22edf6ebebd6ca190af0af657531d8e
Kamal Weheliye <kamalweheliye@gmail.com>
2025-06-10 17:30:01 +0400

    call external api osrm

--- services/trip-service/internal/service/service.go
+++ services/trip-service/internal/service/service.go
@@ -2,8 +2,14 @@ package service
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"io"
+	"log"
+	"net/http"
 	"ride-sharing/services/trip-service/internal/domain"
+	"ride-sharing/shared/types"
 )
 
 type service struct {
@@ -25,4 +31,31 @@ func (s *service) CreateTrip(ctx context.Context, fare *domain.RideFareModel) (*
 	}
 
 	return s.repo.CreateTrip(ctx, t)
+}
+
+func (s *service) GetRoute(ctx context.Context, pickup, destination *types.Coordinate) (*types.OsrmApiResponse, error) {
+	url := fmt.Sprintf(
+		"http://router.project-osrm.org/route/v1/driving/%f,%f;%f,%f?overview=full&geometries=geojson",
+		pickup.Longitude, pickup.Latitude,
+		destination.Longitude, destination.Latitude,
+	)
+	log.Println(url)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("failed to fetch route from OSRM API: %v", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read the response: %v", err)
+	}
+
+	var routeResp types.OsrmApiResponse
+	if err := json.Unmarshal(body, &routeResp); err != nil {
+		return nil, fmt.Errorf("failed to parse response: %v", err)
+	}
+
+	return &routeResp, nil
 }
