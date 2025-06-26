5d30518b4952a141374517ad913b0a1b2ce41157
Kamal Weheliye <kamalweheliye@gmail.com>
2025-06-26 16:58:39 +0400

    feat: add driver

--- services/driver-service/internal/service/service.go
+++ services/driver-service/internal/service/service.go
@@ -1,6 +1,13 @@
 package service
 
-import pb "ride-sharing/shared/proto/driver"
+import (
+	math "math/rand/v2"
+	"ride-sharing/services/driver-service/pkg"
+	pb "ride-sharing/shared/proto/driver"
+	"sync"
+
+	"github.com/mmcloughlin/geohash"
+)
 
 type driverInMap struct {
 	Driver *pb.Driver
@@ -8,10 +15,36 @@ type driverInMap struct {
 
 type Service struct {
 	drivers []*driverInMap
+	mu      sync.RWMutex
 }
 
 func NewService() *Service {
 	return &Service{
 		drivers: make([]*driverInMap, 0),
 	}
+}
+
+// TODO: Register and unregister methods
+
+func (s *Service) RegisterDriver(driverId string, packageSlug string) (*pb.Driver, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	randomIndex := math.IntN(len(pkg.PredefinedRoutes))
+	randomRoute := pkg.PredefinedRoutes[randomIndex]
+
+	geohash := geohash.Encode(randomRoute[0][0], randomRoute[0][1])
+
+	driver := &pb.Driver{
+		Geohash:  geohash,
+		Location: &pb.Location{Latitude: randomRoute[0][0], Longitude: randomRoute[0][1]},
+		Name:     "Lando Norris",
+	}
+
+	return driver, nil
+}
+
+func (s *Service) UnregisterDriver(driverId string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
 }
