b1a60efb2f2ad055123244d996a3b87b5dddd1dc
Kamal Weheliye <kamalweheliye@gmail.com>
2025-07-09 14:37:33 +0400

    feat json and marshalling

--- services/driver-service/internal/infrastructure/messaging/consumer/trip_consumer.go
+++ services/driver-service/internal/infrastructure/messaging/consumer/trip_consumer.go
@@ -2,8 +2,10 @@ package consumer
 
 import (
 	"context"
+	"encoding/json"
 	"github.com/rabbitmq/amqp091-go"
 	"log"
+	"ride-sharing/shared/contracts"
 	"ride-sharing/shared/messaging"
 )
 
@@ -19,7 +21,19 @@ func NewTripConsumer(rabbitmq *messaging.RabbitMQ) *tripConsumer {
 
 func (c *tripConsumer) Listen() error {
 	return c.rabbitmq.ConsumeMessages(messaging.FindAvailableDriversQueue, func(ctx context.Context, msg amqp091.Delivery) error {
-		log.Printf("driver received message: %v", msg)
+		var tripEvent contracts.AmqpMessage
+		if err := json.Unmarshal(msg.Body, &tripEvent); err != nil {
+			log.Printf("Failed to unmarshal message: %v", err)
+			return err
+		}
+
+		var payload messaging.TripEventData
+		if err := json.Unmarshal(tripEvent.Data, &payload); err != nil {
+			log.Printf("Failed to unmarshal message: %v", err)
+			return err
+		}
+
+		log.Printf("driver received message: %+v", payload)
 		return nil
 	})
 }
--- shared/messaging/rabbitmq.go
+++ shared/messaging/rabbitmq.go
@@ -2,10 +2,12 @@ package messaging
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
-	amqp "github.com/rabbitmq/amqp091-go"
 	"log"
 	"ride-sharing/shared/contracts"
+
+	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 const (
@@ -61,7 +63,7 @@ func (r *RabbitMQ) ConsumeMessages(queueName string, handler MessageHandler) err
 	msgs, err := r.Channel.Consume(
 		queueName, // queue
 		"",        // consumer
-		true,      // auto-ack
+		false,     // auto-ack
 		false,     // exclusive
 		false,     // no-local
 		false,     // no-wait
@@ -99,8 +101,14 @@ func (r *RabbitMQ) ConsumeMessages(queueName string, handler MessageHandler) err
 	return nil
 }
 
-func (r *RabbitMQ) PublishMessage(ctx context.Context, routingKey string, message string) error {
+func (r *RabbitMQ) PublishMessage(ctx context.Context, routingKey string, message contracts.AmqpMessage) error {
 	log.Printf("Publishing message with routing key: %s", routingKey)
+
+	jsonMsg, err := json.Marshal(message)
+	if err != nil {
+		return fmt.Errorf("failed to marshal message: %v", err)
+	}
+
 	return r.Channel.PublishWithContext(ctx,
 		TripExchange, // exchange
 		routingKey,   // routing key
@@ -108,7 +116,7 @@ func (r *RabbitMQ) PublishMessage(ctx context.Context, routingKey string, messag
 		false,        // immediate
 		amqp.Publishing{
 			ContentType:  "text/plain",
-			Body:         []byte(message),
+			Body:         jsonMsg,
 			DeliveryMode: amqp.Persistent,
 		})
 }
